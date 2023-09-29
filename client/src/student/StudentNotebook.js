955b7b530e5da3ab95102cb4464833fd0728b16e
Kate Bennert <kate.bennert@gmail.com>
2023-09-29 16:41:02 -0400

    Fix student colors

--- client/src/student/StudentNotebook.js
+++ client/src/student/StudentNotebook.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 function StudentNotebook() {
   return (
-    <div className="container mx-auto p-2 sm:p-4 md:p-6 border border-gray-300 mt-10 rounded-lg bg-textGround">
+    <div className="container mx-auto p-2 sm:p-4 md:p-6 ">
       {/* Tabs */}
       <div className="flex flex-col sm:flex-row">
-        <div className="tab w-full sm:w-1/4 text-center py-2 bg-white rounded-t-lg sm:rounded-l-t-lg sm:rounded-r-none border border-b-0">Reading</div>
+        <div className="tab w-full sm:w-1/4 text-center py-2 bg-textGround rounded-t-lg sm:rounded-l-t-lg sm:rounded-r-none border border-b-0">Reading</div>
         <div className="tab w-full sm:w-1/4 text-center py-2 bg-inactiveGray sm:rounded-none border border-gray-300">Math</div>
         <div className="tab w-full sm:w-1/4 text-center py-2 bg-inactiveGray sm:rounded-none border border-gray-300">Science</div>
         <div className="tab w-full sm:w-1/4 text-center py-2 bg-inactiveGray rounded-t-lg sm:rounded-r-t-lg sm:rounded-l-none border border-gray-300">History</div>
       </div>
 
       {/* Tab Content */}
-      <div className="bg-white p-2 sm:p-4 md:p-6 rounded-b-lg border border-t-0">
+      <div className="bg-textGround p-2 sm:p-4 md:p-6 rounded-b-lg border border-t-0">
         This is the content for Tab 1.
       </div>
 
