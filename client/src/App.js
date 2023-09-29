1d2c249dc3f03ad37042e6c9613d23f16f739675
Kate Bennert <kate.bennert@gmail.com>
2023-09-29 13:08:16 -0400

    Install tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
-import { Route, Routes } from 'react-router-dom';
+// import { useEffect } from "react";
+// import { Route, Routes } from 'react-router-dom';
 
 import React from 'react'
 
 const App = () => {
 
-  useEffect(() => {
-    fetch("/placeholders")
-      .then((r) => r.json())
-      .then((data) => console.log(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/placeholders")
+  //     .then((r) => r.json())
+  //     .then((data) => console.log(data));
+  // }, []);
 
-  return <h1>Hello Hackathon Group 9!</h1>;
+  return <h1 className="text-blue-500">Hello Hackathon Group 9!</h1>;
   
 }
 
