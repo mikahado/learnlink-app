9868f0e8df1cf1b8730647d100c90cfbf6629b13
Kate Bennert <kate.bennert@gmail.com>
2023-09-29 14:10:28 -0400

    Create student dir

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // import { useEffect } from "react";
-// import { Route, Routes } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import React from 'react'
 
@@ -11,8 +11,20 @@ const App = () => {
   //     .then((data) => console.log(data));
   // }, []);
 
-  return <h1 className="text-blue-500">Hello Hackathon Group 9!</h1>;
-  
+  return (
+    <>
+      <h1 className="text-blue-500">Hello Hackathon Group 9!</h1>
+
+      {/* <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/users" element={<UsersPage />} />
+        </Routes>
+      </Router> */}
+
+    </>
+  );
 }
 
 export default App
---
+++ client/src/student/StudentHome.js
@@ -0,0 +1,3 @@
+// gets student info from teacher: useParams()
+// /students/:id
+// teacher can be through student.teacher or user context
