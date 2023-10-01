527b3cc9703ab8895dbe3eb138387ed175a90618
mikahado <maikaharrell@gmail.com>
2023-10-01 12:22:27 -0400

    adds chatgpt to app.py

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 
+import openai
+
 from models import db, Teacher,Student,Subject
 
 app = Flask(__name__)
@@ -14,12 +16,6 @@ CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
-
-# Basic Route for setup 
-# @app.route('/')
-# @app.route('/<int:id>')
-# def index(id=0):
-#     return render_template("index.html")
 
 @app.route('/teacher', methods=['GET'])
 def teacher():
@@ -35,6 +31,43 @@ def teacher():
         jsonify({"text": "Method Not Allowed"}),
         405,
     ) 
+
+@app.route("/moral", methods=("POST",))
+def moral():
+    if request.method == "POST":
+        # need to receive student id and story from the form
+        input_text = request.form["story"]
+        student_id = request.form["student_id"]
+
+        student = Student.query.get(student_id)
+        bio = student.bio
+        prompt = generate_prompt(input_text, bio)
+        # query the database for the student's bio
+
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.9,
+            max_tokens=500
+        )
+
+        return jsonify({"result": response.choices[0].text})
+
+    # result = request.args.get("result")
+    # return render_template("index.html", result=result)
+
+def generate_prompt(input_text, bio):
+    return f"""Story: {input_text} Student bio: {bio}
+    
+    
+Prompt: Summarize the moral of the story above in 280 characters or less for this elementary school student with accessibility needs. Use playful, meaningful emojis to assist in reading comprehension. 6 year old reading level.
+
+Consider the bio of the student and tailor the summary to their needs.
+
+""".format(
+        input_text.capitalize()
+    )
 
 if __name__ == '__main__':
     app.run(port=5555)
