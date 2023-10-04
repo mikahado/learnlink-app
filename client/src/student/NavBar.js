16e7902242958719b4ec3de09fec7584b8856b00
Kate Bennert <kate.bennert@gmail.com>
2023-10-04 04:16:21 -0400

    Create new buttons

--- client/src/student/NavBar.js
+++ client/src/student/NavBar.js
@@ -9,15 +9,21 @@ function NavBar() {
     //const { user, setUser } = useContext(UserContext);
 
     return (
-        <div className="bg-primaryPurple flex justify-between p-4">
-            <Link to={`/students/1`}><button className="py-2 px-4 bg-textGround mx-8 rounded text-buttonTextGreen">Student Name</button></Link>
-            <button className="py-2 px-4 bg-textGround mx-8 rounded text-buttonTextGreen">
-                <span>Help </span>
-                <FontAwesomeIcon icon={faQuestionCircle} size="lg" />
-                {/* <div className="help-button"></div> */}
+        <div className="bg-primaryPurple flex justify-between items-center p-2 ">
+            <Link to={`/students/1`}>
+                <button className="h-12 py-2 px-4 bg-textGround mx-8 rounded-lg text-buttonTextGreen flex items-center justify-center">
+                    Student Name
+                </button>
+            </Link>
+            <button className="h-12 w-32 flex flex-row justify-center items-center rounded-lg bg-textGround text-buttonTextGreen p-4 text-sm">
+                <img src="/hand_icon_125264.png" alt="Help" className="h-8 w-8 mr-2"/>
+                <span>Help</span>
             </button>
-            <button className="py-2 px-4 bg-textGround mx-8 rounded text-buttonTextGreen">Teacher Name</button>
+            <button className="h-12 py-2 px-4 bg-textGround mx-8 rounded-lg text-buttonTextGreen flex items-center justify-center">
+                Teacher Name
+            </button>
         </div>
+
     )
 
 }
