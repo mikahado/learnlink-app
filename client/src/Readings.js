ab9f8d93db88cdc01c784d49218212ca41f7930f
Kate Bennert <kate.bennert@gmail.com>
2023-10-04 00:33:25 -0400

    Add image overlay to student home

--- client/src/Readings.js
+++ client/src/Readings.js
@@ -2,31 +2,31 @@ const readings = [
     {
       id: 1,
       title: "The Hare and the Tortoise",
-      image: "https://fakeimg.pl/600x400",
+      image: "https://i0.wp.com/doodlewash.com/wp-content/uploads/2020/08/Day-5-Tortoise-And-The-Hare-Watercolor-Painting-Illustration.jpg?fit=1024%2C720&ssl=1",
       text: `A Hare was making fun of the Tortoise one day for being so slow. "Do you ever get anywhere?" he asked with a mocking laugh. "Yes," replied the Tortoise, "and I get there sooner than you think. I'll run you a race and prove it." The Hare was much amused at the idea of running a race with the Tortoise, but for the fun of the thing he agreed. So the Fox, who had consented to act as judge, marked the distance and started the runners off. The Hare was soon far out of sight, and to make the Tortoise feel very deeply how ridiculous it was for him to try a race with a Hare, he lay down beside the course to take a nap until the Tortoise should catch up. The Tortoise meanwhile kept going slowly but steadily, and, after a time, passed the place where the Hare was sleeping. But the Hare slept on very peacefully; and when at last he did wake up, the Tortoise was near the goal. The Hare now ran his swiftest, but he could not overtake the Tortoise in time.`
     },
     {
       id: 2,
       title: "The Lion and The Mouse",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3TP8qwDlWRoOt59WUWVFVXyV7eg3whF_2Sw&usqp=CAU",
+      image: "https://cdn-0.fablesofaesop.com/wp-content/uploads/2013/11/i014_th_lion_mouse.jpg",
       text: `A Lion lay asleep in the forest, his great head resting on his paws. A timid little Mouse came upon him unexpectedly, and in her fright and haste to get away, ran across the Lion's nose. Roused from his nap, the Lion laid his huge paw angrily on the tiny creature to kill her. "Spare me!" begged the poor Mouse. 'Please let me go and some day I will surely repay you.' The Lion was much amused to think that a Mouse could ever help him. But he was generous and finally let the Mouse go. Some days later, while stalking his prey in the forest, the Lion was caught in the toils of a hunter's net. Unable to free himself, he filled the forest with his angry roaring. The Mouse knew the voice and quickly found the Lion struggling in the net. Running to one of the great ropes that bound him, she gnawed it until it parted, and soon the Lion was free. "You laughed when I said I would repay you," said the Mouse. "Now you see that even a Mouse can help a Lion."`
     },
     {
       id: 3,
       title: "The Dog and His Reflection",
-      image: "https://fakeimg.pl/600x400",
+      image: "https://img.theepochtimes.com/assets/uploads/2022/01/03/The-Dog-and-His-Reflection-web.jpg",
       text:`A Dog, to whom the butcher had thrown a bone, was hurrying home with his prize as fast as he could go. As he crossed a narrow footbridge, he happened to look down and saw himself reflected in the quiet water as if in a mirror. But the greedy Dog thought he saw a real Dog carrying a bone much bigger than his own. If he had stopped to think he would have known better. But instead of thinking, he dropped his bone and sprang at the Dog in the river, only to find himself swimming for dear life to reach the shore. At last he managed to scramble out, and as he stood sadly thinking about the good bone he had lost, he realized what a stupid Dog he had been.`
     },
     {
       id: 4,
       title: "The Crow and the Pitcher",
-      image: "https://fakeimg.pl/600x400",
+      image: "https://images.nypl.org/index.php?id=407307&t=w",
       text: `The Crow and the Pitcher In a spell of dry weather, when the Birds could find very little to drink, a thirsty Crow found a pitcher with a little water in it. But the pitcher was high and had a narrow neck, and no matter how he tried, the Crow could not reach the water. The poor thing felt as if he must die of thirst. Then an idea came to him. Picking up some small pebbles, he dropped them into the pitcher one by one. With each pebble the water rose a little higher until at last it was near enough so he could drink.`
     },
     {
       id: 5,
       title: "The Ants and the Grasshopper",
-      image: "https://fakeimg.pl/600x400",
+      image: "https://www.dltk-teach.com/fables/grasshopper/grasshopper.gif",
       text: `One bright day in late autumn a family of Ants were bustling about in the warm sunshine, drying out the grain they had stored up during the summer, when a starving Grasshopper, his fiddle under his arm, came up and humbly begged for a bite to eat. "What!" cried the Ants in surprise, "haven't you stored anything away for the winter? What in the world were you doing all last summer?" "I didn't have time to store up any food," whined the Grasshopper; "I was so busy making music that before I knew it the summer was gone." The Ants shrugged their shoulders in disgust. "Making music, were you?" they cried. "Very well; now dance!" And they turned their backs on the Grasshopper and went on with their work.`
     }
 ]
